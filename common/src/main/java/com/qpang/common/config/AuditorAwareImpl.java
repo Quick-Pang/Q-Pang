e8f8da3efa30f7f4bb0eedfea297e14c3395b5ad
Doori Kim <77432549+DDoori@users.noreply.github.com>
2026-03-30 11:22:10 +0900

    feat: 공통 auditing 코드 구현, soft deleted 조회 부분 구현 x 추후 구현 예정

---
+++ common/src/main/java/com/qpang/common/config/AuditorAwareImpl.java
@@ -0,0 +1,16 @@
+package com.qpang.common.config;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class AuditorAwareImpl implements AuditorAware<Long> {
+
+    @Override
+    public Optional<Long> getCurrentAuditor() {
+        //테스트용
+        return Optional.of(1L);
+    }
+}
