d7e81708ba576023ca13da0ef85dfb98004fe680
hhegi <larbillilith@gmail.com>
2026-04-01 11:32:14 +0900

    🍻 test: order-service 테스트 의존성성  분리

--- order-service/src/test/java/com/qpang/orderservice/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/qpang/orderservice/OrderServiceApplicationTests.java
@@ -2,12 +2,16 @@ package com.qpang.orderservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+    "eureka.client.enabled=false",
+    "spring.cloud.discovery.enabled=false"
+})
+@ActiveProfiles("test")
 class OrderServiceApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
