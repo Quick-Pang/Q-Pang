608fc3cbb25783df32d37f041d1439030359b399
Doori Kim <77432549+DDoori@users.noreply.github.com>
2026-04-01 14:31:53 +0900

    refactor: 코드 리뷰 적용

--- product/src/main/java/com/qpang/product/dto/ProductStockRequest.java
+++ product/src/main/java/com/qpang/product/dto/ProductStockRequest.java
@@ -11,5 +11,5 @@ public class ProductStockRequest {
 
     @NotNull
     @Min(1)
-    private int quantity;
+    private Integer quantity;
 }
--- product/src/main/java/com/qpang/product/repository/ProductRepository.java
+++ product/src/main/java/com/qpang/product/repository/ProductRepository.java
@@ -4,10 +4,12 @@ import com.qpang.product.domain.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ProductRepository extends JpaRepository<Product, UUID> {
 
+    Optional<Product> findByIdAndDeletedAtIsNull(UUID id);
     List<Product> findAllByDeletedAtIsNull();
 
     List<Product> findAllByCompanyIdAndDeletedAtIsNull(UUID companyId);
