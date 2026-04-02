2361d60ed1673511b5ec182acbd9fe6bdd4951ed
hhegi <larbillilith@gmail.com>
2026-04-02 17:00:41 +0900

    🍻 test: order-service 테스트용 H2 설정 추가

--- order-service/src/test/java/com/qpang/orderservice/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/qpang/orderservice/OrderServiceApplicationTests.java
@@ -6,7 +6,13 @@ import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(properties = {
     "eureka.client.enabled=false",
-    "spring.cloud.discovery.enabled=false"
+    "spring.cloud.discovery.enabled=false",
+    "spring.datasource.url=jdbc:h2:mem:order_test;MODE=PostgreSQL;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE",
+    "spring.datasource.driver-class-name=org.h2.Driver",
+    "spring.datasource.username=sa",
+    "spring.datasource.password=",
+    "spring.jpa.hibernate.ddl-auto=create-drop",
+    "spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.PostgreSQLDialect"
 })
 @ActiveProfiles("test")
 class OrderServiceApplicationTests {
