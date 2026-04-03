9aa8ded5292424d4dcd54c15cd57cb2d6b7d59c7
YoungJae1118 <apricity1118@gmail.com>
2026-04-03 12:31:46 +0900

    feat: 기능 테스트 완료

--- common/src/main/java/com/qpang/common/config/AuditorAwareImpl.java
+++ common/src/main/java/com/qpang/common/config/AuditorAwareImpl.java
@@ -4,13 +4,14 @@ import org.springframework.data.domain.AuditorAware;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
+import java.util.UUID;
 
 @Component
-public class AuditorAwareImpl implements AuditorAware<Long> {
+public class AuditorAwareImpl implements AuditorAware<UUID> {
 
     @Override
-    public Optional<Long> getCurrentAuditor() {
-        //테스트용
-        return Optional.of(1L);
+    public Optional<UUID> getCurrentAuditor() {
+        // 테스트용 고정 UUID
+        return Optional.of(UUID.fromString("11111111-1111-1111-1111-111111111111"));
     }
 }
