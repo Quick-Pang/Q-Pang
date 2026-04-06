e55d0a77309b6cb5e3964753fd24d89f2a19c710
RoastedBear <kinim329@gmail.com>
2026-04-06 14:05:46 +0900

    feat: 허브 redis연결

--- hub/src/main/java/com/qpang/hub/application/service/HubRouteService.java
+++ hub/src/main/java/com/qpang/hub/application/service/HubRouteService.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.cache.annotation.Cacheable;
 
 @Service
 @RequiredArgsConstructor
@@ -17,6 +18,7 @@ public class HubRouteService {
     private final HubRepository hubRepository;
 
     @Transactional(readOnly = true)
+    @Cacheable(value = "hubRouteList", key = "#pageable.pageNumber + '-' + #pageable.pageSize")
     public Page<HubRouteResponseDto> getAllRoutes(Pageable pageable) {
         return hubRouteRepository.findAll(pageable)
                 .map(HubRouteResponseDto::from);
