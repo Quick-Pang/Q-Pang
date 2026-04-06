e26cd17c4d1fe012f9c5fdadde1ad04e6a963079
hhegi <larbillilith@gmail.com>
2026-04-07 00:29:21 +0900

    🩹 fix: 공개 경로를 실제 경로에 맞게 수정

--- gateway-service/src/main/java/com/qpang/gateway/JwtAuthenticationGlobalFilter.java
+++ gateway-service/src/main/java/com/qpang/gateway/JwtAuthenticationGlobalFilter.java
@@ -57,7 +57,7 @@ public class JwtAuthenticationGlobalFilter implements GlobalFilter, Ordered {
 
     private static final List<String> PUBLIC_PREFIXES = List.of(
             "/actuator",
-            "/auth/sign-up"
+            "/auth/"
     );
 
     @Override
